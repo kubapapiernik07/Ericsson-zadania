2833a2266c8ab75357341e4c535fea43b859cd8d
kubapapiernik07 <43754303+kubapapiernik07@users.noreply.github.com>
2021-07-22 21:58:10 +0200

    Update task3.cpp

--- task3.cpp
+++ task3.cpp
@@ -2,13 +2,13 @@
 
 using namespace std;
 
-unsigned long long fibonacci(int n)
+unsigned long long fibonacci(int n) //variable for biggest values
 {
     if (n < 2) return n;
     return fibonacci(n-1) + fibonacci(n-2);
 }
 
-unsigned long long factorial(int f)
+unsigned long long factorial(int f) //variable for biggest values
 {
     if (f < 2) return 1;
     return f * factorial(f-1);
@@ -17,13 +17,13 @@ unsigned long long factorial(int f)
 int main ()
 {
     cout << "Fibonacci sequence result is: ";
-    for (int n = 0; n < 94; n++)
+    for (int n = 0; n < 94; n++)    //result for n>=94 exceed value of unsigned long long
     {
         cout << fibonacci(n) << " ";
     }
     cout << endl << endl << "Factorial result is: ";
 
-    for (int f = 0; f < 21; f++)
+    for (int f = 0; f < 21; f++)    //result for f>=21 exceed value of unsigned long long
     {
         cout << factorial(f) << " ";
     }
